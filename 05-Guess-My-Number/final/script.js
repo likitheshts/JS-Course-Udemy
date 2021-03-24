30f2d089a123a46bbd856ec3df47b729f8823a86
likithesh <likitheshts444@gmail.com>
2021-03-24 19:09:16 +0530

    Update script.js

--- 05-Guess-My-Number/final/script.js
+++ 05-Guess-My-Number/final/script.js
@@ -1,29 +1,29 @@
 'use strict';
 
 const secret = Math.trunc(Math.random() * 20) + 1;
-
+const display = (message) => {
+  document.querySelector('.message').textContent = message;
+};
 document.querySelector('.check').addEventListener('click', () => {
   const num = Number(document.querySelector('.guess').value);
   if (!num) {
-    document.querySelector('.message').textContent = 'No Number Entered!!';
+    // document.querySelector('.message').textContent = 'No Number Entered!!';
+    display('No Number Entered!!');
   } else if (num === secret) {
-    document.querySelector('.message').textContent = 'Correct Number';
+    display('Correct Number');
     let sc2 = Number(document.querySelector('.score').textContent);
     document.querySelector('.score').textContent = sc2 + 1;
     document.querySelector('.number').textContent = secret;
-    document.querySelector('.highscore').textContent = sc2+1;
-    
+    document.querySelector('.highscore').textContent = sc2 + 1;
+
     //styles
     document.querySelector('body').style.backgroundColor = 'green';
     document.querySelector('.number').style.width = '30rem';
-  } else if (num > secret) {
-    document.querySelector('.message').textContent = 'Too Highhhhh';
+  } else if (num !== secret) {
+    document.querySelector('.message').textContent =
+      num > secret ? 'Too Highhhhh' : 'Tooo loww';
     let sc = Number(document.querySelector('.score').textContent);
     document.querySelector('.score').textContent = sc - 1;
-  } else if (num < secret) {
-    document.querySelector('.message').textContent = 'Too Lowwww';
-    let sc1 = Number(document.querySelector('.score').textContent);
-    document.querySelector('.score').textContent = sc1 - 1;
   }
 });
 
